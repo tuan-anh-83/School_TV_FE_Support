68db3641606acacfe982fd88153f15f3319a6c76
supercoderl <150410325+supercoderl@users.noreply.github.com>
2025-05-29 16:43:59 +0700

    ui

--- src/pages/WatchLive/WatchLive.jsx
+++ src/pages/WatchLive/WatchLive.jsx
@@ -1096,7 +1096,7 @@ const WatchLive = () => {
                   <iframe
                     src={`${toIframeUrl(
                       displayIframeUrl
-                    )}?autoplay=1&mute=0&controls=${currentStatus === "Live" || currentStatus === "LateStart" ? 0 : 1}&rel=0&playsinline=1`}
+                    )}?autoplay=1&mute=0&controls=0&rel=0&playsinline=1`}
                     allow="autoplay; encrypted-media; fullscreen;"
                     allowFullScreen
                     className="youtube-player"
